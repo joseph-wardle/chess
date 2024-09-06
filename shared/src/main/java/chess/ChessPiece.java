37add3bcab36bf6e4185c04463bd95acd302cfdc
joseph-wardle <joseph.m.wardle@gmail.com>
2024-09-06 00:08:18 -0600

    Implemented all ChessPiece moves

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -3,7 +3,6 @@ package chess;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * Represents a single chess piece
@@ -12,8 +11,8 @@ import java.util.Set;
  * signature of the existing methods.
  */
 public class ChessPiece {
-    private ChessGame.TeamColor color;
-    private PieceType type;
+    private final ChessGame.TeamColor color;
+    private final PieceType type;
 
     public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
         this.color = pieceColor;
@@ -54,22 +53,14 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        switch (type) {
-            case KING:
-                return kingMoves(board, myPosition);
-            case QUEEN:
-                return queenMoves(board, myPosition);
-            case BISHOP:
-                return bishopMoves(board, myPosition);
-            case KNIGHT:
-                return knightMoves(board, myPosition);
-            case ROOK:
-                return rookMoves(board, myPosition);
-            case PAWN:
-                return pawnMoves(board, myPosition);
-            default:
-                throw new RuntimeException("Unknown piece type");
-        }
+        return switch (type) {
+            case KING -> kingMoves(board, myPosition);
+            case QUEEN -> queenMoves(board, myPosition);
+            case BISHOP -> bishopMoves(board, myPosition);
+            case KNIGHT -> knightMoves(board, myPosition);
+            case ROOK -> rookMoves(board, myPosition);
+            case PAWN -> pawnMoves(board, myPosition);
+        };
     }
 
     private Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition) {
@@ -98,23 +89,141 @@ public class ChessPiece {
     }
 
     private Collection<ChessMove> queenMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new HashSet<>();
+        moves.addAll(rookMoves(board, myPosition));
+        moves.addAll(bishopMoves(board, myPosition));
+        return moves;
     }
 
     private Collection<ChessMove> bishopMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new HashSet<>();
+        int[][] directions = {
+                {-1, -1}, {-1,  1},
+                { 1, -1}, { 1,  1}
+        };
+
+        for (int[] direction : directions) {
+            for (int i = 1; i < 8; i++) {
+                int newRow = myPosition.getRow() + direction[0] * i;
+                int newColumn = myPosition.getColumn() + direction[1] * i;
+                try {
+                    ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+                    ChessPiece piece = board.getPiece(newPosition);
+                    if (piece == null || piece.getTeamColor() != color) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                    }
+                    if (piece != null) {
+                        break;
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Ignore invalid positions
+                }
+            }
+        }
+
+        return moves;
     }
 
     private Collection<ChessMove> knightMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new HashSet<>();
+        int[][] directions = {
+                {-2, -1}, {-2,  1},
+                {-1, -2}, {-1,  2},
+                { 1, -2}, { 1,  2},
+                { 2, -1}, { 2,  1}
+        };
+
+        for (int[] direction : directions) {
+            int newRow = myPosition.getRow() + direction[0];
+            int newColumn = myPosition.getColumn() + direction[1];
+            try {
+                ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null || piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, newPosition));
+                }
+            } catch (IllegalArgumentException e) {
+                // Ignore invalid positions
+            }
+        }
+
+        return moves;
     }
 
     private Collection<ChessMove> rookMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new HashSet<>();
+        int[][] directions = {
+                     {-1,  0},
+                { 0, -1}, { 0,  1},
+                     { 1,  0}
+        };
+
+        for (int[] direction : directions) {
+            for (int i = 1; i < 8; i++) {
+                int newRow = myPosition.getRow() + direction[0] * i;
+                int newColumn = myPosition.getColumn() + direction[1] * i;
+                try {
+                    ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+                    ChessPiece piece = board.getPiece(newPosition);
+                    if (piece == null || piece.getTeamColor() != color) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                    }
+                    if (piece != null) {
+                        break;
+                    }
+                } catch (IllegalArgumentException e) {
+                    // Ignore invalid positions
+                }
+            }
+        }
+
+        return moves;
     }
 
     private Collection<ChessMove> pawnMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new HashSet<>();
+        int direction = color == ChessGame.TeamColor.WHITE ? 1 : -1;
+        int startRow = color == ChessGame.TeamColor.WHITE ? 2 : 7;
+        int newRow = myPosition.getRow() + direction;
+        int newColumn = myPosition.getColumn();
+        try {
+            ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+            ChessPiece piece = board.getPiece(newPosition);
+            if (piece == null) {
+                moves.add(new ChessMove(myPosition, newPosition));
+                if (myPosition.getRow() == startRow) {
+                    newRow = myPosition.getRow() + 2 * direction;
+                    newPosition = new ChessPosition(newRow, newColumn);
+                    piece = board.getPiece(newPosition);
+                    if (piece == null) {
+                        moves.add(new ChessMove(myPosition, newPosition));
+                    }
+                }
+            }
+        } catch (IllegalArgumentException e) {
+            // Ignore invalid positions
+        }
+
+        int[][] attackDirections = {
+                {-1,  direction},
+                { 1,  direction}
+        };
+
+        for (int[] attackDirection : attackDirections) {
+            newRow = myPosition.getRow() + attackDirection[0] * direction;
+            newColumn = myPosition.getColumn() + attackDirection[1] * direction;
+            try {
+                ChessPosition newPosition = new ChessPosition(newRow, newColumn);
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece != null && piece.getTeamColor() != color) {
+                    moves.add(new ChessMove(myPosition, newPosition));
+                }
+            } catch (IllegalArgumentException e) {
+                // Ignore invalid positions
+            }
+        }
+
+        return moves;
     }
 
     @Override
