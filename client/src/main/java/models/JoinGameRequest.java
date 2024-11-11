af12245530b8f053f42fe82a9af720b98038d1be
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-11 09:34:44 -0700

    Finished JoinGameRequest

--- client/src/main/java/models/JoinGameRequest.java
+++ client/src/main/java/models/JoinGameRequest.java
@@ -1,4 +1,18 @@
 package models;
 
 public class JoinGameRequest {
+    private String playerColor;
+    private int gameID;
+
+    public void setPlayerColor(String playerColor, int gameID) {
+        this.playerColor = playerColor;
+        this.gameID = gameID;
+    }
+
+    public String getPlayerColor() {
+        return playerColor;
+    }
+    public int getGameID() {
+        return gameID;
+    }
 }
