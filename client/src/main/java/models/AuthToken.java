9d4b7babb13a65b51f936167170478b20a561bbb
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-11 09:29:14 -0700

    Finished AuthToken

--- client/src/main/java/models/AuthToken.java
+++ client/src/main/java/models/AuthToken.java
@@ -1,4 +1,19 @@
 package models;
 
 public class AuthToken {
+    private String token;
+    private String username;
+
+    public AuthToken(String token, String username) {
+        this.token = token;
+        this.username = username;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
