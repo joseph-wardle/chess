2b5d70e83b986a3aac865ecee731d69325df50c2
joseph-wardle <joseph.m.wardle@gmail.com>
2024-11-11 09:33:23 -0700

    Finished Game model

--- client/src/main/java/models/Game.java
+++ client/src/main/java/models/Game.java
@@ -1,4 +1,31 @@
 package models;
 
 public class Game {
+    private int gameID;
+    private String gameName;
+    private String whiteUsername;
+    private String blackUsername;
+
+    public Game(int gameID, String gameName, String whiteUsename, String blackUsername) {
+        this.gameID = gameID;
+        this.gameName = gameName;
+        this.whiteUsername = whiteUsename;
+        this.blackUsername = blackUsername;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+
+    public String getGameName() {
+        return gameName;
+    }
+
+    public String getWhiteUsername() {
+        return whiteUsername;
+    }
+
+    public String getBlackUsername() {
+        return blackUsername;
+    }
 }
