2aec8c994bc418d84cf2b72ccca8a37848153b84
joseph-wardle <joseph.m.wardle@gmail.com>
2024-12-09 14:51:35 -0700

    More miscellaneous changes

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,7 +1,7 @@
 import ui.UIController;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         UIController controller = new UIController();
         controller.run();
     }
--- server/src/main/java/services/WebSocketService.java
+++ server/src/main/java/services/WebSocketService.java
@@ -3,29 +3,6 @@ package services;
 import org.eclipse.jetty.websocket.api.annotations.*;
 import org.eclipse.jetty.websocket.api.Session;
 
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
-
 @WebSocket
 public class WebSocketService {
-    private static AuthService authService;
-    private static GameService gameService;
-
-    private static final Map<Integer, Set<Session>> gameSessions = new ConcurrentHashMap<>();
-    private static final Map<Session, Integer> sessionGameMap = new ConcurrentHashMap<>();
-    private static final Map<Session, String> sessionUserMap = new ConcurrentHashMap<>();
-
-    public static void initialize(AuthService auth, GameService game) {
-        authService = auth;
-        gameService = game;
-    }
-
-    @OnWebSocketConnect
-    public void onConnect(Session session) {
-        System.out.println("WebSocket opened: " + session.getRemoteAddress().getAddress());
-    }
-
-    @OnWebSocketMessage
-
 }
